a4adb7466bc523bb45e6754fb65d3eeb4534bf51
MakrisJr <s2036401@ed.ac.uk>
2025-11-07 16:21:37 +0000

    Add greyscale UNet

--- model.py
+++ model.py
@@ -1801,3 +1801,76 @@ class SegNet(nn.Module):
         # for name in th:
             # s_dict[corresp_name[name]] = th[name]
         self.load_state_dict(th)
+
+class UNet2(nn.Module):
+    def __init__(self, in_channels=1, out_channels=1):  # Adjusted for 4 classes
+        super(UNet2, self).__init__()
+        self.n_classes = out_channels
+
+        def conv_block(in_c, out_c):
+            return nn.Sequential(
+                nn.Conv2d(in_c, out_c, kernel_size=3, padding=1),
+                nn.ReLU(inplace=True),
+                nn.Conv2d(out_c, out_c, kernel_size=3, padding=1),
+                nn.ReLU(inplace=True)
+            )
+        
+        def upsample(in_c, out_c):
+            return nn.Sequential(
+                nn.ConvTranspose2d(in_c, out_c, kernel_size=2, stride=2),
+                nn.ReLU(inplace=True)
+            )
+        
+        self.enc1 = conv_block(in_channels, 64)
+        self.enc2 = conv_block(64, 128)
+        self.enc3 = conv_block(128, 256)
+        self.enc4 = conv_block(256, 512)
+        
+        self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
+        
+        self.bottleneck = conv_block(512, 1024)
+        
+        self.up4 = upsample(1024, 512)
+        self.dec4 = conv_block(1024, 512)
+        self.up3 = upsample(512, 256)
+        self.dec3 = conv_block(512, 256)
+        self.up2 = upsample(256, 128)
+        self.dec2 = conv_block(256, 128)
+        self.up1 = upsample(128, 64)
+        self.dec1 = conv_block(128, 64)
+        
+        self.final = nn.Conv2d(64, out_channels, kernel_size=1)
+
+        self.apply(self._initialize_weights)
+        print("Initialized weights with Kaiming Normal")
+
+        
+    def forward(self, x):
+        e1 = self.enc1(x)
+        e2 = self.enc2(self.pool(e1))
+        e3 = self.enc3(self.pool(e2))
+        e4 = self.enc4(self.pool(e3))
+        
+        b = self.bottleneck(self.pool(e4))
+        
+        d4 = self.dec4(torch.cat((self.up4(b), e4), dim=1))
+        d3 = self.dec3(torch.cat((self.up3(d4), e3), dim=1))
+        d2 = self.dec2(torch.cat((self.up2(d3), e2), dim=1))
+        d1 = self.dec1(torch.cat((self.up1(d2), e1), dim=1))
+        
+        return self.final(d1)
+    
+    def _initialize_weights(self, m): 
+        if isinstance(m, nn.Conv2d) or isinstance(m, nn.ConvTranspose2d):
+            # print(f"Initializing {m}")
+            nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+            if m.bias is not None:
+                nn.init.constant_(m.bias, 0)
+        elif isinstance(m, nn.BatchNorm2d):
+            # print(f"Initializing BatchNorm: {m}")
+            nn.init.constant_(m.weight, 1)
+            nn.init.constant_(m.bias, 0)
+        elif isinstance(m, nn.ReLU) or isinstance(m, nn.Sequential) or isinstance(m, UNet2) or isinstance(m, nn.MaxPool2d):
+            pass
+        else:
+            print(f"Warning: {m} not initialized")
---
+++ new-train.py
@@ -0,0 +1,249 @@
+'''
+This code is written by:
+
+Dr. Vivek Kumar Singh
+Department of Computer Science
+Newcastle University, United Kingdom
+Date: 24/August/2021
+
+Also, thanks to "https://github.com/milesial/" for utilzing some of their codes.
+
+'''
+import argparse
+import logging
+import os
+import sys
+import copy
+import time
+import numpy as np
+import torch
+import torch.nn as nn
+from torch import optim
+from tqdm import tqdm
+from torchvision import transforms
+from eval import eval_net
+from model import U_Net,AttU_Net,LinkNetImprove,U2NETP,R2U_Net,DeepLabv3_plus,FCN,SegNet, UNet2
+from transformation import ELM_transform, ELM_transform_gray
+from tensorboardX import SummaryWriter
+from dataset import BasicDataset
+from torch.utils.data import DataLoader, random_split
+from dice_loss import Dice_Loss
+import torch.nn.functional as F
+from efficientunet import *
+import matplotlib.pyplot as plt
+import csv
+
+#------------- Load the images from directory----------
+train_dir_img = './train/image/'
+train_dir_mask = './train/mask/'
+val_dir_img = './val/image/'
+val_dir_mask = './val/mask/'
+dir_checkpoint = './checkpoint/'
+# ------------------Loading END ------------------------
+
+n_classes =1
+n_channels = 1
+MODEL_NAME = f'ELM_{time.strftime("%b-%d-%Y_%H%M")}.model'
+
+def train_net(net,
+              device,
+              epochs=15,
+              batch_size=4,
+              lr=0.0001,
+              val_percent=0.1,
+              save_cp=True,
+              img_scale=1):
+    
+    transform = ELM_transform_gray()
+    train_dataset = BasicDataset(train_dir_img, train_dir_mask, img_scale,transform = transform['train'], single_channel=True)
+
+    val_dataset= BasicDataset(val_dir_img, val_dir_mask, img_scale,transform['val'], single_channel=True)
+    n_train=len(train_dataset)
+    n_val=len(val_dataset)
+
+# ----------- Load the dataset from the directory---------
+
+    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=2, pin_memory=True)
+    val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False, num_workers=2, pin_memory=True, drop_last=True)
+
+
+
+    writer = SummaryWriter(logdir=f'{MODEL_NAME}/logs', comment=f'LR_{lr}_BS_{batch_size}_SCALE_{img_scale}')
+    print('TensorBoard logs writing to:', writer.logdir)
+    global_step = 0
+
+    logging.info(f'''Starting training:
+        Epochs:          {epochs}
+        Batch size:      {batch_size}
+        Learning rate:   {lr}
+        Training size:   {n_train}
+        Validation size: {n_val}
+        Checkpoints:     {save_cp}
+        Device:          {device.type}
+        Images scaling:  {img_scale}
+    ''')
+
+# !!---------- Defined the optimizer --------------------------!!
+
+    optimizer = optim.Adam(net.parameters(), lr = lr, betas=(0.9, 0.999), eps=1e-08, weight_decay=1e-9)
+    scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, 'min' if net.n_classes > 1 else 'max', patience=10)
+
+# ------------------ Loss function ------------------!!
+    criterion_dice = Dice_Loss
+    criterion = nn.BCEWithLogitsLoss().cuda()
+    
+# !!-------------- Training and validation loop ------------------!!
+    loss_index = []
+    loss_values = []
+    List_Loss = []
+    best_acc=0
+    for epoch in range(epochs):
+        net.train()
+
+        epoch_loss = 0
+        with tqdm(total=n_train, desc=f'Epoch {epoch + 1}/{epochs}', unit='img') as pbar:
+            for ibatch, batch in enumerate(train_loader):
+                imgs = batch['image']
+                true_masks = batch['mask']
+                imgs = imgs.to(device=device, dtype=torch.float32)
+                mask_type = torch.float32 if net.n_classes == 1 else torch.long
+                true_masks = true_masks.to(device=device, dtype=mask_type)
+
+                masks_pred = net(imgs)
+                out_new =  F.sigmoid(masks_pred)
+
+                # masks_probs_flat = masks_pred.view(-1)
+                # true_masks_flat = true_masks.view(-1)
+                loss = 0.5*criterion(masks_pred, true_masks) + 0.5*criterion_dice(out_new, true_masks)
+                epoch_loss += loss.item()
+                writer.add_scalar('Loss/', loss.item(), global_step)
+                pbar.set_postfix(**{'loss (batch)': loss.item()})
+
+                optimizer.zero_grad()
+                loss.backward()
+                nn.utils.clip_grad_value_(net.parameters(), 0.1)
+                optimizer.step()
+
+                loss_index.append(epoch) 
+                loss_values.append(epoch_loss)   
+                List_Loss.append([epoch, epoch_loss])
+
+                if epoch == 0 and ibatch == 1:
+                    print_gpu_mem(device)
+                pbar.update(imgs.shape[0])
+                global_step += 1
+                if global_step % ((n_val+n_train) // (10 * batch_size)) == 0:
+                    for tag, value in net.named_parameters():
+                        tag = tag.replace('.', '/')
+                        writer.add_histogram('weights/' + tag, value.data.cpu().numpy(), global_step)
+                        #writer.add_histogram('grads/' + tag, value.grad.data.cpu().numpy(), global_step)
+                    val_score = eval_net(net, val_loader, device)
+                    scheduler.step(val_score)
+                    writer.add_scalar('learning_rate', optimizer.param_groups[0]['lr'], global_step)
+
+                    if net.n_classes > 1:
+                        logging.info('Validation cross entropy: {}'.format(val_score))
+                        writer.add_scalar('Loss/test', val_score, global_step)
+                    else:
+                        logging.info('Validation Dice Coeff: {}'.format(val_score))
+                        writer.add_scalar('Dice/test', val_score, global_step)
+                    
+                    if val_score > best_acc:
+                        best_acc = val_score
+                        best_model_wts = copy.deepcopy(net.state_dict())
+
+                    writer.add_images('images', imgs, global_step)
+                    if net.n_classes == 1:
+                        writer.add_images('masks/true', true_masks, global_step)
+                        writer.add_images('masks/pred', torch.sigmoid(masks_pred) > 0.5, global_step)
+
+    if save_cp:
+        try:
+            os.mkdir(dir_checkpoint)
+            logging.info('Created checkpoint directory')
+        except OSError:
+            pass
+        net.load_state_dict(best_model_wts)
+        # t = time.localtime()
+        # timestamp = time.strftime('%b-%d-%Y_%H%M', t)
+        torch.save(net.state_dict(), '{}{}.model'.format(dir_checkpoint, MODEL_NAME))
+
+    writer.close()
+
+def get_args():
+    parser = argparse.ArgumentParser(description='2D ELM line segmentation from OCT images',
+                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('-e', '--epochs', metavar='E', type=int, default=100,
+                        help='Number of epochs', dest='epochs')
+    parser.add_argument('-b', '--batch-size', metavar='B', type=int, nargs='?', default=2,
+                        help='Batch size', dest='batchsize')
+    parser.add_argument('-l', '--learning-rate', metavar='LR', type=float, nargs='?', default=0.0002,
+                        help='Learning rate', dest='lr')
+    parser.add_argument('-f', '--load', dest='load', type=str, default=False,
+                        help='Load model from a .pth file')
+    parser.add_argument('-s', '--scale', dest='scale', type=float, default=1,
+                        help='Downscaling factor of the images')
+    parser.add_argument('-v', '--validation', dest='val', type=float, default=15.0,
+                        help='Percent of the data that is used as validation (0-100)')
+
+    return parser.parse_args()
+
+
+def mb(x): return x / 1024**2
+
+def print_gpu_mem(device=None, prefix=''):
+    if not torch.cuda.is_available():
+        print(prefix + 'No CUDA device')
+        return
+    if device is None:
+        device = torch.cuda.current_device()
+    torch.cuda.synchronize(device)
+    allocated = torch.cuda.memory_allocated(device)        # memory currently allocated by tensors
+    reserved  = torch.cuda.memory_reserved(device)         # memory managed by the caching allocator
+    max_alloc  = torch.cuda.max_memory_allocated(device)   # peak allocated by tensors
+    print(f"{prefix}GPU {device} allocated: {mb(allocated):.1f} MB, "
+          f"reserved: {mb(reserved):.1f} MB, peak_alloc: {mb(max_alloc):.1f} MB")
+    # optional: a readable summary
+    print(torch.cuda.memory_summary(device=device, abbreviated=True))
+
+
+
+if __name__ == '__main__':
+
+    logging.basicConfig(level=logging.INFO, format='%(levelname)s: %(message)s')
+    args = get_args()
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    logging.info(f'Using device {device}')   
+
+# -------------- Load the model -----------
+    #net = get_efficientunet_b3(n_classes=1, concat_input=True, pretrained=True)
+    #net = LinkNetImprove(n_channels=3, n_classes=1)
+    #net = AttU_Net(n_channels=3, n_classes=1)
+    #net = U_Net(n_channels=3, n_classes=1)
+    #net = R2U_Net(n_channels=3, n_classes=1,t=2)
+    #net = DeepLabv3_plus(n_channels=3, n_classes=1, os=16, pretrained=True, _print=True)
+    #net = FCN(n_channels=3, n_classes=1)
+    # net = SegNet(n_channels=3, n_classes=1)
+    net = UNet2(1,1)
+
+    if args.load:
+        net.load_state_dict(
+            torch.load(args.load, map_location=device)
+        )
+        logging.info(f'Model loaded from {args.load}')
+    net.to(device=device)
+    try:
+        train_net(net=net,
+                  epochs=args.epochs,
+                  batch_size=args.batchsize,
+                  lr=args.lr,
+                  device=device,
+                  img_scale=args.scale,
+                  val_percent=args.val / 100)
+    except KeyboardInterrupt:
+        torch.save(net.state_dict(), f'{MODEL_NAME}/INTERRUPTED.pth')
+        logging.info('Saved interrupt')
+        try:
+            sys.exit(0)
+        except SystemExit:
+            os._exit(0)
